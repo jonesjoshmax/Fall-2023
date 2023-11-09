71668c81c868d180768ea4734fb3d3c760d1d96a
jonesjoshmax <jonesjoshmax@gmail.com>
2023-11-09 16:04:05 -0500

    [flight mech] a5 initial commit

---
+++ Flight Mechanics/A5/A5.py
@@ -0,0 +1,67 @@
+import numpy as np
+from Vertexes import Vertexes
+from Visualizer import Visualizer
+from Dynamics import Dynamics
+from ForcesMoments import ForcesMoments
+from Trim import Trim
+from Autopilot import Autopilot
+from Rotations import Euler2Rotation as rot
+import Parameters as p
+
+vertexes = Vertexes(p.model)
+verts = vertexes.stl_to_vertices() * p.scale
+verts = np.matmul(verts, rot(0, 0, -np.pi / 2))
+
+fm = ForcesMoments()
+dyn = Dynamics()
+ap = Autopilot(50, 10)
+anim = Visualizer()
+trim = Trim()
+
+# TIME INITIALIZATION
+t0 = p.t0
+t1 = p.t1
+ts = p.ts
+t = t0
+
+Va = 35
+angle = np.deg2rad(0)
+radius = np.inf
+state, defL = trim.minTrim(Va, angle, radius)
+
+anim.initialization()
+dyn.state = np.copy(state)
+
+while t < t1:
+    if t <= 100:
+        Va_r = 30
+        h_r = 100
+        chi_r = np.deg2rad(0)
+    elif 100 < t <= 200:
+        Va_r = 45
+        h_r = 250
+        chi_r = np.deg2rad(0)
+    else:
+        Va_r = 60
+        h_r = 500
+        chi_r = np.deg2rad(10)
+    Va = np.sqrt(dyn.state[3, 0] ** 2 + dyn.state[4, 0] ** 2 + dyn.state[5, 0] ** 2)
+
+    u = np.array([t, dyn.state[6, 0], dyn.state[7, 0], dyn.state[8, 0], dyn.state[9, 0], dyn.state[10, 0],
+                 dyn.state[11, 0], Va, dyn.state[2, 0], Va_r, h_r, chi_r])
+    defL = ap.update(u)
+
+    force, lmn = fm.compute(dyn.state, defL)
+    dyn.update(force, lmn)
+
+    if p.temp % p.plt == 0:
+        dPack = np.array([Va, Va_r, h_r, chi_r])
+        ppp = dyn.state[(0, 1, 2), 0]
+        ph, th, ps = dyn.state[(6, 7, 8), 0].flatten()
+        r = rot(-ph, th, ps)
+        rVerts = np.matmul(r, verts.T).T
+        tVerts = rVerts + ppp
+        anim.update(tVerts, dyn.state, force, lmn, defL, dPack, t)
+
+    t += ts
+    p.temp += 1
---
+++ Flight Mechanics/A5/AutoGains.py
@@ -0,0 +1,80 @@
+import numpy as np
+from Parameters import *
+from MAV import MAV
+from Trim import Trim
+
+mav = MAV()
+gamma = mav.gamma
+
+trim = Trim()
+Va = 35
+angle = np.deg2rad(0)
+radius = np.inf
+xTrim, uTrim = trim.minTrim(Va, angle, radius)
+
+VaT = np.sqrt(xTrim[3, 0] ** 2 + xTrim[4, 0] ** 2 + xTrim[5, 0] ** 2)
+aT = np.arctan(xTrim[5, 0] / xTrim[3, 0])
+bT = np.arctan(xTrim[4, 0] / VaT)
+thT = xTrim[7, 0]
+
+g0 = gamma[0]
+g3 = gamma[3]
+g4 = gamma[4]
+
+cpp = g3 * cLp + g4 * cNp
+cPDa = g3 * cLDa + g4 * cNDa
+aPh1 = -.5 * rho * VaT ** 2 * S * b * cpp * (b / (2 * VaT))
+aPh2 = .5 * rho * VaT ** 2 * S * b * cPDa
+aTh1 = -(rho * VaT ** 2 * c * S) / (2 * jy) * cmq * (c / (2 * VaT))
+aTh2 = -(rho * VaT ** 2 * c * S) / (2 * jy) * cma
+aTh3 = (rho * VaT ** 2 * c * S) / (2 * jy) * cmDe
+aV1 = ((rho * VaT * S) / M) * (cd0 + (cda * aT) + (cdDe * uTrim[0, 0])) + (rho * S_prop) / M * cProp * VaT
+aV2 = (rho * S_prop) / M * cProp * kMotor ** 2 * uTrim[3, 0]
+aV3 = g * np.cos(thT - aT)
+
+z = 0.707
+
+# ROLL
+trRoll = 1
+wRoll = 2.2 / trRoll
+kdRoll = (2 * z * wRoll - aPh1) / aPh2
+kpRoll = wRoll ** 2 / aPh1
+kiRoll = 0
+
+# COURSE
+trCourse = 2.2
+wCourse = 2.2 / trCourse
+zCourse = 0.5
+kdCourse = 0
+kpCourse = (2 * zCourse * wCourse * VaT) / g
+kiCourse = (wCourse ** 2 * VaT) / g
+
+# PITCHD
+zPitch = 0.1
+trPitch = 0.1
+wPitch = 2.2 / trPitch
+kdPitch = (2 * zPitch * wPitch - aTh1) / aTh3
+kpPitch = (wPitch ** 2 - aTh2) / aTh3
+kiPitch = 0
+kDCth = kpPitch * aTh3 / (aTh2 + kpPitch * aTh3)
+
+# ALTITUDE
+trAlt = 1.0
+wAlt = 2.2 / trAlt
+kdAlt = 0
+kpAlt = 2 * z * wAlt / (kDCth * VaT)
+kiAlt = wAlt ** 2 / (kDCth * VaT)
+
+# AIRSPEED
+trVa = 1.0
+wVa = 2.2 / trVa
+kdVa = 0
+kpVa = (aV1 - 2 * z * wVa) / kDCth
+kiVa = wVa ** 2 / (kDCth * g)
+
+# THROTTLE
+trThr = 0.1
+wThr = 2.2 / trThr
+kdThr = 0
+kpThr = (2 * z * wThr - aV1) / aV2
+kiThr = wThr ** 2 / aV2
